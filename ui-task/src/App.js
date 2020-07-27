5bff1f679be0155e6a16b6fd999ed323b19d01ce
Rumit Mehta <68600838+Rumit-Mehta@users.noreply.github.com>
2020-07-27 19:44:29 +0100

    Added Services Table - first draft

--- ui-task/src/App.js
+++ ui-task/src/App.js
@@ -4,12 +4,30 @@ import AssignServicesButton from "./Components/AssignServicesButton";
 import CancelButton from "./Components/CancelButton";
 import Heading from "./Components/Heading";
 import ButtonTable from "./Components/ButtonTable";
+import ServicesTable from "./Components/ServicesTable";
 
 function App() {
   return (
     <div>
       <Heading />
       <ButtonTable elevation={0} />
+      <ServicesTable
+        data={[]}
+        header={[
+          {
+            name: "id",
+            prop: "id",
+          },
+          {
+            name: "Service",
+            prop: "service",
+          },
+          {
+            name: "Description",
+            prop: "description",
+          },
+        ]}
+      />
     </div>
   );
 }
